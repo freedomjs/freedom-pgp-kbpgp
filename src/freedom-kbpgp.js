c67b59d75411ea8b62b270803eefe76577c8f225
soycode <soycode@users.noreply.github.com>
2014-09-29 11:10:06 -0700

    Can see kbpgp from freedom on Chrome

--- src/freedom-kbpgp.js
+++ src/freedom-kbpgp.js
@@ -1,14 +1,12 @@
-/*globals freedom:true, fdom, console, require, global*/
+/*globals freedom:true, fdom, console, require, global, kbpgp*/
 
 /**
  * Implementation of a crypto-pgp provider for freedom.js
  * using the Keybase PGP implementation, kbpgp.
  **/
 
-//var kbpgp = require('kbpgp');
-
 // Global declarations for node.js
-/*if (typeof global !== 'undefined') {
+if (typeof global !== 'undefined') {
   if (typeof window === 'undefined') {
     global.window = {};
   }
@@ -22,9 +20,13 @@
   if (typeof XMLHttpRequest === 'undefined') {
     XMLHttpRequest = {};
   }
-}*/
+}
 
+console.log(kbpgp.box);  // proof that we can see kbpgp
+
+// TODO - the actual module below
 var fdomkb = function() {
+  this.kbpgp = kbpgp;
 };
 
 /** REGISTER PROVIDER **/
