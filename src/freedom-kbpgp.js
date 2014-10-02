40753b0e00d61046420f1bb3c27363711ea637dc
soycode <soycode@users.noreply.github.com>
2014-10-02 14:15:23 -0700

    Splitting demo from API, some initial implementation
    This commit leaves a particular TODO that is blocking for kbpgp
    in freedom - specifically, window.crypto/crypto.getRandomValues
    needs to be visible from the WebWorker using kbpgp. Since these
    are native code they are not normally visible in WebWorkers.
    
    Suggested workaround: put crypto/getRandomValues in freedom core.
    This will make the dependency asynchronous, but at least it should
    be there. Once this is done, this repo can be revisited with an eye
    to finishing out and testing the kbpgp -> API methods layer.

--- src/freedom-kbpgp.js
+++ src/freedom-kbpgp.js
@@ -1,4 +1,4 @@
-/* globals freedom:true, console, require, global, kbpgp */
+/* globals freedom:true, console, require, global, kbpgp, crypto */
 /* jslint indent:2,white:true,sloppy:true */
 
 /**
@@ -6,21 +6,88 @@
  * using the Keybase PGP implementation, kbpgp.
  **/
 
-console.log(kbpgp.box);  // proof that we can see kbpgp
+// native code that is visible in this scope, needed in webworker by kbpgp
+// TODO: make core.crypto (or getRandomValues) and link window back to it
+console.log(crypto.getRandomValues);
 
-// TODO - the actual module below
 var fdomkbpgp = function() {
   this.kbpgp = kbpgp;
 };
 
-fdomkbpgp.prototype.initialize = function(initOpts) {
-  this.keypair = kbpgp.KeyManager.generate_ecc(
-    { userid : initOpts.userid },
+fdomkbpgp.prototype.initialize = function(opts) {
+  this.passphrase = opts.passphrase;  // TODO hash!
+  this.keypair = this.kbpgp.KeyManager.generate_ecc(
+    { userid : opts.userid },
     function(err, user) {
       user.sign({}, function(err) {
         console.log('ecc key generated!');
       });
     });
+};
+
+fdomkbpgp.prototype.encrypt = function(opts) {
+  this.kbpgp.box(
+    { msg: opts.data, encrypt_for: opts.key },
+    function(err, result_string, result_buffer) {
+      console.log(err, result_string, result_buffer);
+    });
+};
+
+fdomkbpgp.prototype.decrypt = function(opts) {
+};
+
+fdomkbpgp.prototype.sign = function(opts) {
+  this.kbpgp.box(
+    { msg: opts.data, sign_with: this.keypair },
+    function(err, result_string, result_buffer) {
+      console.log(err, result_string, result_buffer);
+    });
+};
+
+fdomkbpgp.prototype.verify = function(opts) {
+};
+
+fdomkbpgp.prototype.signEncrypt = function(opts) {
+  this.kbpgp.box(
+    { msg: opts.data, encrypt_for: opts.key, sign_with: this.keypair },
+    function(err, result_string, result_buffer) {
+      console.log(err, result_string, result_buffer);
+    });
+};
+
+fdomkbpgp.prototype.decryptVerify = function(opts) {
+  var ring = new kbpgp.keyring.KeyRing();
+  ring.add_key_manager(this.keypair);
+  ring.add_key_manager(opts.verifyingKey);
+  kbpgp.unbox(
+    { keyfetch: ring, armored: opts.data },
+    function(err, literals) {
+      if (err !== null) {
+        console.log('Problem: ' + err);
+      } else {
+        console.log('decrypted message');
+        console.log(literals[0].toString());
+        var ds, km = null;
+        ds = literals[0].get_data_signer();
+        if (ds) { km = ds.get_key_manager(); }
+        if (km) {
+          console.log("Signed by PGP fingerprint");
+          console.log(km.get_pgp_fingerprint().toString('hex'));
+        }
+      }
+    });
+};
+
+fdomkbpgp.prototype.importKey = function(opts) {
+};
+
+fdomkbpgp.prototype.exportKey = function(opts) {
+};
+
+fdomkbpgp.prototype.wrapKey = function(opts) {
+};
+
+fdomkbpgp.prototype.unwrapKey = function(opts) {
 };
 
 /** REGISTER PROVIDER **/
